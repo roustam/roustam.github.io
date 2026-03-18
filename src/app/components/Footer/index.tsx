6a3327bf24fcfacee2daeb3ae4eba5061d2100be
Roustam Dzhafarov <roustam.ts@gmail.com>
2026-03-18 20:11:39 +0400

    Update footer year

--- src/app/components/Footer/index.tsx
+++ src/app/components/Footer/index.tsx
@@ -3,7 +3,7 @@ export default function SiteFooter() {
     
     return (
         <div className=" flex flex-row justify-center w-full mb-2">
-            <p> roustam.github.io @ 2024 </p> 
+            <p> roustam.github.io @ 2026 </p> 
         </div>
     )
 }
